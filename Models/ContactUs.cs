230f49defb3f84745c48152f02fa404c57becdf6
George <jvillaloboscr@gmail.com>
2015-06-24 01:23:42 -0400

    added contact us page 2

--- Models/ContactUs.cs
+++ Models/ContactUs.cs
@@ -11,12 +11,15 @@ namespace test_mvc_website.App_Data
 
         public Guid Id { get; set; }
         [Required]
+        [Display(Name="Name")]
         public string Name { get; set; } // name
         [Required]
         [EmailAddress(ErrorMessage = "invalid email")]
+        [Display(Name="Email")]
         public string Email { get; set; } // email
 
         [Required]
+        [Display(Name="Your Message")]
         public string Message { get; set; }
     }
 }
